dfbec1d1ed4c7f4c26379c3b6bfe07d8098c6345
vega2kvibe2 <vega2kvibe2@gmail.com>
2026-05-14 16:48:52 +0900

    TodoHeader add

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        TodoHeader
+        <h1>TODO it!</h1>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
+    setup() {
+
 
         return {}
     }
@@ -15,5 +15,9 @@ export default {
 </script>
 
 <style scoped>
-
+h1 {
+    color: #2F3852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
