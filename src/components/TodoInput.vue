3c1ab08b39ed8f04f3e1b1f1acd5a79dc59817b6
vega2kvibe2 <vega2kvibe2@gmail.com>
2026-05-14 17:22:05 +0900

    TodoInput add

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
-        TodoInput
+        <input type="text" autofocus :value="newTodoItem" @input="handleInput">
+        <button @click="addTodo">추가</button>
     </div>
-</template>
+</template>``
 
 <script setup>
+import { ref } from 'vue'
 
+const emit = defineEmits(["input:todo"])
+
+const newTodoItem = ref("")
+
+const handleInput = (event) => {
+    const todoText = event.target.value
+    if (!todoText) return
+    emit("input:todo", todoText)
+    newTodoItem.value = todoText
+}
+const addTodo = () => {
+    const todoItem = newTodoItem.value
+    localStorage.setItem(todoItem, todoItem)
+    newTodoItem.value = ""
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
