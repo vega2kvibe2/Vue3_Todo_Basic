2bbec909e0b145ce9856b88336007827304e501c
vega2kvibe2 <vega2kvibe2@gmail.com>
2026-05-15 09:28:18 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,74 @@
 <template>
     <div>
-        TodoList
+        <ul>
+            <li v-for="(item, index) in todoItems" :key="index" class="shadow">
+                {{ item }}
+                <span class="removeBtn" @click="removeTodo(item, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+const todoItems = ref([])
+
+onBeforeMount(() => {
+    console.log('mounted in the composition api!')
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageKey = localStorage.key(i)
+            todoItems.value.push(storageKey)
+        }
+    }
+    console.log(todoItems.value)
+})
+
+const removeTodo = (todoItem, index) => {
+    localStorage.removeItem(todoItem)
+    todoItems.value.splice(index, 1)
+}
 
 </script>
 
 <style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
 
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+
+.checkBtnCompleted {
+    color: #b3adad;
+}
+
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
 </style>
